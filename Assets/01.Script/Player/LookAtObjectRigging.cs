5c60e3a9f55d312e586e7085b0bd1328a358123c
sehyeon1104 <sehyeon1104@naver.com>
2022-06-19 04:33:41 +0900

    애니메이션

---
+++ Assets/01.Script/Player/LookAtObjectRigging.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations.Rigging;
+public class LookAtObjectRigging : MonoBehaviour
+{
+    Rig rig;
+    public float targetWeight;
+    private void Awake()
+    {
+      rig=  GetComponent<Rig>();
+    }
+    private void Update()
+    {
+        rig.weight = Mathf.Lerp(rig.weight, targetWeight, Time.deltaTime*10f);
+
+        if(Input.GetKeyDown(KeyCode.T))
+        {
+
+        }
+    }
+}
